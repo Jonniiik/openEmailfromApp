c5f25dc196d34989bf8b2b8cff6d58b32c41df2c
Eugene <e.ponomarev@mstrike.ru>
2020-10-27 09:39:09 +0300

    Передача данных в почту, и удаление переданного файла в почту

--- app/src/main/java/com/example/openemail/MainActivity.java
+++ app/src/main/java/com/example/openemail/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.openemail;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,13 +19,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Objects;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView email;
+
+    /**
+     * По данному коду определяем, отправилось письмо или нет
+     */
+    private static final int LOG_PERMISSION_CODE = 4000;
+
+    private String fileLOGName;
+
+    private File fileLOG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,52 +44,83 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
 
         email.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
             @Override
             public void onClick(View view) {
-                File root = Environment.getExternalStorageDirectory();
-                File file = new File(root, "exception-report.txt");
+
+                //Определяем текущее время, для того что б вставить в имя файла
+                Date dt = Calendar.getInstance().getTime();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd_MM_HH_mm_ss");
+
+                //Присваиваем имя файла, нужно быть аккуратным, из за некоторых символов, файл может быть не создаться
+                fileLOGName = "report_log_" + simpleDateFormat.format(dt) + ".txt";
+                //Создаем файл
+                fileLOG = new File(getExternalFilesDir("txt"), fileLOGName);
 
                 try {
                     PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-                    FileOutputStream flogs = new FileOutputStream(file);
+                    FileOutputStream flogs = new FileOutputStream(fileLOG);
                     flogs.write((
+                            "\n" +
+                            "\n" +
+                            "-----------------------------------------------\n" +
                             "Time: " + Calendar.getInstance().getTime() + "\n" +
-                                    "OS: " + Build.VERSION.RELEASE + "\n" +
-                                    "SDK_VERSION: " + Build.VERSION.SDK_INT + "\n" +
-                                    "Model: " + Build.MODEL + "\n" +
-                                    "Версия приложения: " + pInfo.versionName + "\n" + "\n" +
-                                    "Arrays.toString(exception.getStackTrace())" //Текст ошибки
-                    ).getBytes());
+                            "OS: " + Build.VERSION.RELEASE + "\n" +
+                            "SDK_VERSION: " + Build.VERSION.SDK_INT + "\n" +
+                            "Model: " + Build.MODEL + "\n" +
+                            "Версия ТНС ЭНЕРГО: " + pInfo.versionName + "\n" + "\n" +
+                            "\n" +
+                            "Информация об ошибке\n" +
+                            "-----------------------------------------------\n" +
+                            "Arrays.toString(exception.getStackTrace())").getBytes());
+                    flogs.flush();
                     flogs.close();
-                } catch (FileNotFoundException | PackageManager.NameNotFoundException ex) {
-                    ex.printStackTrace();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-
-                if (!file.exists() || !file.canRead()){
-                    Toast.makeText(MainActivity.this, "Произошла ошибка прикрепления файла", Toast.LENGTH_SHORT).show();
-                    return;
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
 
                 String[] emails = new String[]{"zhek90@gmail.com"};
 
+                Uri logUri = FileProvider.getUriForFile(MainActivity.this, getApplicationContext().getPackageName() + ".fileprovider", fileLOG);
+
                 Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("plain/text");
+
                 intent.putExtra(Intent.EXTRA_EMAIL, emails);
-                intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Тема");
+                //intent.putExtra(Intent.EXTRA_TEXT, bodyText);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_STREAM, logUri);
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
 
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     try {
-                        startActivity(Intent.createChooser(intent, "Chose apps"));
+                        startActivityForResult(Intent.createChooser(intent, "Chose app"), LOG_PERMISSION_CODE);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                         Toast.makeText(MainActivity.this, "Произошла ошибка", Toast.LENGTH_SHORT).show();
                     }
                 }
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LOG_PERMISSION_CODE) {
+            //Место для удаления файла
+            deleteFile(fileLOGName);
+            if (fileLOG.delete()) {
+                Toast.makeText(MainActivity.this, "Файл удалён", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Файл НЕ удалён", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
